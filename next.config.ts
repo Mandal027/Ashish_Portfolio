61292c19bf452a9fdd22f7902616b0afc60ed909
Nilesh <kumarnilesh010524@gmail.com>
2025-05-29 07:13:31 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+base: process.env.NEXT_BASE_PATH || "/Ashish_Portfolio",
 };
 
 export default nextConfig;
